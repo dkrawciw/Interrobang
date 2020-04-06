b548f99532e3af3b510f4663c70ce7762b03cc99
Daniel Krawciw <46691574+dkrawciw@users.noreply.github.com>
2020-04-06 15:16:20 -0600

    The basic look of the website with basic back-end

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("yeet");
+var express = require('express'),
+    app = express(),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require('./models/user.js');
+
+mongoose.connect('mongodb://localhost:27017/tsaWebApp', {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use(require("express-session")({
+  secret: "p*51nvfy8ZX8NI@M*x7BkUg37BhVpP85v8M3z^%qBcyBrUpLct!^eV6^ejd702c0hcAYe&SM*lEadf9XIxT%IBw0l&yQOSAOn8f",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}
+
+app.listen(80, function(){
+  console.log('Server Running');
+});
+
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
